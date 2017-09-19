1788d044c59f9768def0519fe779970e3eb2f6fa
Alec <reyer101@mail.chapman.edu>
2017-09-18 17:15:32 -0700

    Intital commit
    Intitial commit

---
+++ Scrolls/Assets/Prefabs/Player/Scripts/AlecController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+/*
+Name: Alec Reyerson
+ID: 1826582
+Email: reyer101@mail.chapman.edu
+Course: CPSC-344-01
+Assignment: Alpha Milestone
+
+Description: Script for handling player input
+*/
+
+// CharacterController
+public class AlecController : MonoBehaviour {
+    private PlayerCharacter m_Player;    
+    private bool m_Jump, m_Crouched;
+    private Vector3 m_CrouchScale, m_NormalScale;
+
+    // Awake
+    void Awake () {
+        m_Player = GetComponent<PlayerCharacter>();       
+        m_NormalScale = gameObject.transform.localScale;
+        m_CrouchScale = gameObject.transform.localScale * .667f;                       		
+	}
+	
+	// Update
+	void Update () {
+        if (!m_Jump)
+        {
+            m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+        }
+
+        if(CrossPlatformInputManager.GetButtonDown("Crouch"))
+        {
+            Debug.Log("Crouch pressed");
+            if(m_Crouched)
+            {
+                gameObject.transform.localScale = m_NormalScale;
+                m_Crouched = false;
+            } 
+            else
+            {
+                gameObject.transform.localScale = m_CrouchScale;
+                m_Crouched = true;
+            }                        
+        }
+    }
+
+    // FixedUpdate
+    void FixedUpdate()
+    {
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        m_Player.Move(h, m_Jump);
+        m_Jump = false;
+
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+        m_Player.Climb(v);
+    }
+}
---
+++ Scrolls/Assets/Prefabs/Player/Scripts/PlayerCharacter.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Name: Alec Reyerson
+    ID: 1826582
+    Email: reyer101@mail.chapman.edu
+    Course: CPSC-344-01
+    Assignment: Alpha Milestone
+
+    Description: Script for performing actions based on character input.
+    */
+
+// CharacterController
+public class PlayerCharacter : MonoBehaviour {
+    public float m_MaxSpeed, m_ClimbSpeed, m_JumpForce;    
+    private bool m_Grounded, m_CanClimb;
+    private Rigidbody2D m_Rigidbody2D;
+    private Transform m_GroundCheck, m_ClimbCheck;
+    private CircleCollider2D m_CircleCollider2D;
+    private LayerMask m_LayerMask;
+
+    private float k_GroundedRadius = .5f;
+    private float k_ClimbRadius = .15f;
+
+    // Awake
+    void Awake () {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        m_GroundCheck = transform.Find("GroundCheck");
+        m_ClimbCheck = transform.Find("ClimbCheck");
+        m_LayerMask = 1;        	
+	}
+	
+	// FixedUpdate
+	void FixedUpdate () {
+        m_Grounded = false;
+        m_CanClimb = false;
+        m_Rigidbody2D.gravityScale = 2;       
+
+        // Check if player is standing on ground by searching for colliders overlapping radius at bottom of player
+        Collider2D[] gColliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_LayerMask);
+        for (int i = 0; i < gColliders.Length; i++)
+        {            
+            if (gColliders[i].gameObject != gameObject)  // If a collider besides the one attatched to the player is found
+                m_Grounded = true;                      // then the player is considerd to be grounded           
+        }
+
+        Collider2D[] cColliders = Physics2D.OverlapCircleAll(m_ClimbCheck.position, k_ClimbRadius, m_LayerMask);
+        for (int i = 0; i < cColliders.Length; i++)
+        {
+            if (cColliders[i].gameObject.tag.Contains("Climb"))
+            {  
+                Debug.Log("Overlapping object: " + cColliders[i].gameObject.name);
+                m_CanClimb = true;
+            }               
+        }
+    }
+
+    /*
+    Name: Move
+    Parameters: float horizontal, bool jump
+    */
+    public void Move(float horizontal, bool jump)
+    {        
+        m_Rigidbody2D.velocity = new Vector2(horizontal * m_MaxSpeed, m_Rigidbody2D.velocity.y);        
+        if (m_Grounded && jump)
+        {            
+            m_Grounded = false;                
+            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+        }
+    }
+
+    /*
+    Name: Climb
+    Parameters: float vertical
+    */
+    public void Climb(float vertical)
+    {
+        Debug.Log("Can climb: " + m_CanClimb);
+        if (m_CanClimb)
+        {
+            m_Rigidbody2D.gravityScale = 0;
+            m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, vertical * m_ClimbSpeed);
+        }
+    }
+
+    /* 
+    Name: OnTriggerEnter2D
+    Parameters: Collider 
+    */
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag.Contains("Climb")) {
+            m_CanClimb = true;
+        }
+    }
+
+    /* 
+   Name: OnTriggerExit2D
+   Parameters: Collider 
+   */
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Contains("Climb")) {
+            m_CanClimb = false;
+        }
+    }
+}
