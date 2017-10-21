152e617dae1a2cb7d8ef3446833812adc5854012
carissa27 <smith426@mail.chapman.edu>
2017-10-21 14:00:08 -0700

    Commit 10/21/2017
    Player info scene added (temp art for now, set up a couple new scripts for UI sort of stuff)

---
+++ Scrolls/Assets/Scripts/CS/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿//Carissa Smith
+//1800292
+//smith426@mail.chapman.edu
+//CPSC-344-01
+//Scrolls
+//This script's purpose is to load scenes within the game. 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	public string sceneName;
+
+	//load
+	public void load() {
+		SceneManager.LoadScene(sceneName);
+	}
+}
+
