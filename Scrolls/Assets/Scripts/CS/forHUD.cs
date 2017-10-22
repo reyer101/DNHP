6d7bf9286da805500bc853bb68112c3e8ce4acf2
Alec <reyer101@mail.chapman.edu>
2017-10-22 15:04:41 -0700

    Removed unnecessary files
    - Removed unnecessary files
    - Added some of Carissa's changes

---
+++ Scrolls/Assets/Scripts/CS/forHUD.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class forHUD : MonoBehaviour {
+
+	public Toggle wizTog;
+	public Toggle witTog;
+
+	public Dropdown nameSelect;
+
+	public int nameIndex;
+	public int spriteIndex;
+
+	public void getName()
+	{
+		nameIndex = nameSelect.value;
+		PlayerPrefs.SetInt ("Name", nameIndex);
+		
+	}
+
+	public void getSprite()
+	{
+		if (wizTog.isOn == true) 
+		{
+			spriteIndex = 0;
+			PlayerPrefs.SetInt ("Sprite", spriteIndex);
+		} 
+
+		else if (witTog.isOn == true)
+		{
+			spriteIndex = 1;
+			PlayerPrefs.SetInt ("Sprite", spriteIndex);
+		}
+	}
+}
